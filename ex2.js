f7111944364f42a5209addca8e0ef797197ba6c8
Pranil01 <94604003+Pranil01@users.noreply.github.com>
2023-06-18 22:35:14 +0530

    Add files via upload

---
+++ ex2.js
@@ -0,0 +1,20 @@
+var x = 20
+x =34.5
+console.log(x)
+x=true
+x="hello"
+console.log(x)
+var w =3400
+p=`${w}`
+console.log(p)
+S=` this is a string that spans
+multiple line so it is a
+multi line string use
+backtick `
+console.log(S)
+L=[11,222,33,44,55]
+console.log("L=",L ," type of L is ",typeof(L))
+var a="saket"
+console.log("a=",a,"type of a is ",typeof(a))
+var k
+console.log("a=",a,"type of a is ",typeof(a))
---
+++ ex3.js
@@ -0,0 +1,10 @@
+let p=10
+
+console.log(p)
+
+const x = 100
+let s=200
+//console.log(s)
+s=30
+console.log(s)
+console.log(x)//Value of const can not changed
